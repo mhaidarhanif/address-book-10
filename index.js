f8de9702548cae29cd5f459cd2c3f2b53521cf7b
M Haidar Hanif <git@mhaidarhanif.com>
2025-10-04 16:15:26 +0700

    contacts data and display it

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 console.log("Address Book (Batch 10)");
 
-const contact = "Alpaca";
-const contacts = ["Alpaca", "Bear", "Cat"];
+const contacts = [
+  {
+    id: 1,
+    fullName: "M Haidar Hanif",
+    phone: "+62-812-345-678",
+    email: "haidar@haidar.com",
+    address: "Bandung, Indonesia",
+  },
+  {
+    id: 2,
+    fullName: "Elon Musk",
+    phone: "+1-2345-678",
+    email: "elon@elon.com",
+    address: "California, US",
+  },
+  {
+    id: 3,
+    fullName: "Steve Jobs",
+    phone: "+1-2345-678",
+    email: "jobs@jobs.com",
+    address: "Somewhere, US",
+  },
+];
+
+console.log(contacts);
