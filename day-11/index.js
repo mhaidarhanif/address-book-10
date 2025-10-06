5ca509bf9223ac5afc86ed4dba2f0e704c6d52e7
M Haidar Hanif <git@mhaidarhanif.com>
2025-10-06 22:10:29 +0700

    feat: describe animal function example

--- day-11/index.js
+++ day-11/index.js
@@ -1,5 +1,14 @@
 console.log("Day 11");
 
-for (let number = 0; number < 3; number++) {
-  console.log(number);
+const exampleAnimal = {
+  name: "Bear",
+  color: "brown",
+  speed: 56, // km/h
+};
+
+function describeAnimal(animal) {
+  console.log(`${animal.name} has ${animal.color} color,
+with running speed up to ${animal.speed} km/h`);
 }
+
+describeAnimal(exampleAnimal);
