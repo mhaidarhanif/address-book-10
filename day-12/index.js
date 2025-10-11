6cf42228ff054592650b41135eafb551a0b858b8
M Haidar Hanif <git@mhaidarhanif.com>
2025-10-11 14:35:20 +0700

    spread operator

--- day-12/index.js
+++ day-12/index.js
@@ -7,3 +7,42 @@ const numbers = [1, 2, 3];
 const continuedNumbers = [...numbers, 4, 5, 6];
 
 console.log(continuedNumbers);
+
+// --------------------------------------------------------------------
+
+const contact = {
+  name: "Haidar",
+  email: "haidar@haidar.com",
+};
+
+const updatedContact = {
+  ...contact,
+  phone: "+6281234567890",
+};
+
+console.log(updatedContact);
+
+// --------------------------------------------------------------------
+
+const contacts = [
+  {
+    id: 1,
+    name: "Haidar",
+    email: "haidar@haidar.com",
+  },
+  {
+    id: 2,
+    name: "Hanif",
+    email: "hanif@hanif.com",
+  },
+];
+
+const newContactData = {
+  id: 3,
+  name: "Elon",
+  email: "elon@elon.com",
+};
+
+const updatedContacts = [...contacts, newContactData];
+
+console.log(updatedContacts);
